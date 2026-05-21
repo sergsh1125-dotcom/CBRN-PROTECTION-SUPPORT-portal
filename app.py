7029a5bfeeaeae54a172c5459adb53105cf71a58
sergsh1125-dotcom <serg.sh.1125@gmail.com>
2026-05-21 15:52:18 +0300

    Clarify info message about coordinate loading
    Updated the info message to clarify that coordinates are loaded from the actual radiation situation map.

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ with col_left:
     st.link_button("1.3. Карта радіаційного моніторингу країн ЄС", "https://remap.jrc.ec.europa.eu/Advanced.aspx")
     st.link_button("1.4. Карта прогнозу хімічної обстановки", "http://forecast.inf.ua/")
     st.link_button("1.5. Карта фактичної РХБ обстановки", "https://map-obstanovka-vuvukyx4vwu9jrhuv68vcg.streamlit.app/")
-    st.info("💡 Координати завантажуються кліком мишки.")
+    st.info("💡 Координати на карті фактичної РХБ обстановки завантажуються кліком мишки.")
 
     st.markdown('<p class="module-header">МОДУЛЬ 2. БАЗИ ДАНИХ</p>', unsafe_allow_html=True)
     st.link_button("2.1. Аварійні картки НХР", "https://sergsh1125-dotcom.github.io/emergency-cards/")
