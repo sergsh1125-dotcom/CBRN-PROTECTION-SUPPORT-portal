1daaa3d652124879d54a92faa910fe02ea9973cd
sergsh1125-dotcom <serg.sh.1125@gmail.com>
2026-05-21 17:24:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
-
+import os
 # --- 1. НАЛАШТУВАННЯ СТОРІНКИ ---
 st.set_page_config(
     page_title="ОФІС CBRN",
@@ -231,3 +231,47 @@ with col_right:
     st.link_button("4.1. Укргідрометеоцентр", "https://www.meteo.gov.ua/")
     st.link_button("4.2. Нормативно-правова база РХЗ", "https://dsns.gov.ua/zakonodavstvo/perelik-normativno-pravovix-dokumentiv-shho-reglamentuyut-diyalnist-pidrozdiliv-dsns-ukrayini/upravlinnia-organizaciyi-radiaciinogo-ximicnogo-ta-biologicnogo-zaxistu")
     st.link_button("4.3. СОП 1.1/РХБЗ: Демеркуризація\nСОП 1.2: Дії підрозділів при НС з НХР", "https://kyiv.dsns.gov.ua/navchalniy-centr-gu/sluzhbova-pidgotovka/normativno-pravovi-akti")
+    # ===============================
+# 4.4. ФОРМАЛІЗОВАНІ ДОКУМЕНТИ
+# ===============================
+
+DOCS_FOLDER = "docx"
+
+show_docs = st.button(
+    "4.4. ФОРМАЛІЗОВАНІ ДОКУМЕНТИ",
+    use_container_width=True
+)
+
+if show_docs:
+
+    st.markdown("### 📂 Файли DOCX")
+
+    if os.path.exists(DOCS_FOLDER):
+
+        doc_files = sorted([
+            f for f in os.listdir(DOCS_FOLDER)
+            if f.endswith(".docx")
+        ])
+
+        if doc_files:
+
+            for file_name in doc_files:
+
+                file_path = os.path.join(DOCS_FOLDER, file_name)
+
+                with open(file_path, "rb") as file:
+
+                    st.download_button(
+                        label=f"📄 {file_name}",
+                        data=file,
+                        file_name=file_name,
+                        mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                        use_container_width=True,
+                        key=file_name
+                    )
+
+        else:
+            st.warning("У папці docx немає файлів.")
+
+    else:
+        st.error("Папка docx не знайдена.")
